fcb71dc6770ce3edefba20a6b8eb70a4b60b85c7
Khushal <k.m.sherki25@gmail.com>
2026-03-08 23:56:33 +0530

    Added solution to compute minimum sum after splitting digits of a four digit number

---
+++ MinimumSplitDigitSumCalculator.java
@@ -0,0 +1,31 @@
+/**
+ * ------------------------------------------------------------
+ * Problem : Minimum Sum of Four Digit Number After Splitting Digits
+ * Class   : MinimumSplitDigitSumCalculator
+ * ------------------------------------------------------------
+ */
+
+import java.util.Arrays;
+
+class MinimumSplitDigitSumCalculator {
+
+    public int minimumSum(int num) {
+
+        int[] digits = new int[4];
+
+        // Extract digits
+        for (int i = 0; i < 4; i++) {
+            digits[i] = num % 10;
+            num /= 10;
+        }
+
+        // Sort digits
+        Arrays.sort(digits);
+
+        // Form two smallest numbers
+        int new1 = digits[0] * 10 + digits[2];
+        int new2 = digits[1] * 10 + digits[3];
+
+        return new1 + new2;
+    }
+}
